c3d16ff6e1c412b8c81fb387d6e242a4f4eaf7b5
Vladislav1300 <askerko1997@gmail.com>
2022-08-01 11:43:19 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const objBrackets = {};
+  const openBrackets = [];
+  
+  let stack = [];
+  let arrFromStr = Array.from(str);  
+
+  bracketsConfig.forEach(elem => {
+    openBrackets.push(elem[0]);
+  })
+
+  bracketsConfig.forEach((elem) => {
+    objBrackets[elem[1]]= elem[0];
+  })
+
+  arrFromStr.forEach(currentSymbol => {
+    if (openBrackets.includes(currentSymbol) && !(Object.keys(objBrackets).includes(currentSymbol))) {
+      stack.push(currentSymbol);
+    }
+    else {
+      if (stack.length === 0) return false;
+      if (objBrackets[currentSymbol] === stack[stack.length -1]) stack.pop();
+      else return false;
+    }
+  })
+  
+  if (stack.length === 0) return true;
+  else return false;
 }
